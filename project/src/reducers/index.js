0dfadf778ef1b50dbabfa706c6b0ff975a67b3c4
Christopher Barrett <municipalshock@gmail.com>
2020-10-06 18:20:06 -0700

    src index set and provider

---
+++ project/src/reducers/index.js
@@ -0,0 +1,10 @@
+// set initial state
+const initialState = {
+  players: [],
+  isFetching: false,
+  error: "",
+};
+
+const reducer = (state = initialState, action) => {};
+
+// pass our state and action
